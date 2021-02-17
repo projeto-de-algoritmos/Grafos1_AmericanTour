4237cf4531176f8ab44c184419f15d15e304a30b
Ithalo Azevedo <ithaloazevedoo@gmail.com>
2021-02-17 14:48:37 -0300

    Populate cities.js

--- world-tour/src/cities.js
+++ world-tour/src/cities.js
@@ -1,132 +1,147 @@
 export const cities = [
-  {
-    name: 'Washington',
-    coordinates: [-77.03655831116859, 38.91862635897642],
-    markerOffset: -15,
-  },
-  {
-    name: 'Miami',
-    coordinates: [-80.20268202730227, 25.764768118455407],
-    markerOffset: -15,
-  },
-  {
-    name: 'Nova York',
-    coordinates: [-73.94401297179279, 40.7356621309984],
-    markerOffset: -15,
-  },
-  {
-    name: 'Toronto',
-    coordinates: [-79.60809330413308, 43.68489624121235],
-    markerOffset: -15,
-  },
-  {
-    name: 'Vancouver',
-    coordinates: [-123.11360638784217, 49.280340393447624],
-    markerOffset: -15,
-  },
-  {
-    name: 'Montreal',
-    coordinates: [-73.58680236572978, 45.56853263294665],
-    markerOffset: -15,
-  },
-  {
-    name: 'Rio de Janeiro',
-    coordinates: [-43.18921923638575, -22.927798424412373],
-    markerOffset: -15,
-  },
-  {
-    name: 'São Paulo',
-    coordinates: [-46.633793633809084, -23.568923445514507],
-    markerOffset: 25,
-  },
-  {
-    name: 'Amsterdã',
-    coordinates: [4.842841226614125, 52.35416345772578],
-    markerOffset: -15,
-  },
-  {
-    name: 'Paris',
-    coordinates: [2.330578325070535, 48.85680480202678],
-    markerOffset: -15,
-  },
-  {
-    name: 'Berlim',
-    coordinates: [13.346423870986895, 52.48561127089402],
-    markerOffset: -15,
-  },
-  {
-    name: 'Glasglow',
-    coordinates: [-4.273664480004037, 55.86270262628466],
-    markerOffset: -15,
-  },
-  {
-    name: 'Londres',
-    coordinates: [-0.14074530098284344, 51.497790976427304],
-    markerOffset: -15,
-  },
-  {
-    name: 'Manchester',
-    coordinates: [-2.2363434875430106, 53.45890148011352],
-    markerOffset: -15,
-  },
-  {
-    name: 'Oslo',
-    coordinates: [10.761077452175183, 59.91340704946014],
-    markerOffset: -15,
-  },
-  {
-    name: 'Tokyo',
-    coordinates: [139.73261791204246, 35.66258399079574],
-    markerOffset: -15,
-  },
-  {
-    name: 'Yokohama',
-    coordinates: [139.64316996367347, 35.438647434637765],
-    markerOffset: -15,
-  },
-  {
-    name: 'Kumamoto',
-    coordinates: [130.71405375274375, 32.791160718889174],
-    markerOffset: -15,
-  },
-  {
-    name: 'Hong Kong',
-    coordinates: [114.18811352358868, 22.323447816962556],
-    markerOffset: -15,
-  },
-  {
-    name: 'Pequim',
-    coordinates: [116.38643462562045, 39.88061000499358],
-    markerOffset: -15,
-  },
-  {
-    name: 'Xangai',
-    coordinates: [121.45186402647722, 31.204342182428128],
-    markerOffset: -15,
-  },
-  {
-    name: 'Seoul',
-    coordinates: [126.99228676204655, 37.54433183264052],
-    markerOffset: -15,
-  },
-  {
-    name: 'Cidade do Cabo',
-    coordinates: [18.353715012552726, -34.14548723743959],
-    markerOffset: -15,
-  },
-  {
-    name: 'Nairobi',
-    coordinates: [36.81167940997817, -1.3038914951063236],
-    markerOffset: -15,
-  },
-  {
-    name: 'Angola',
-    coordinates: [13.228827895004587, -8.823925433761437],
-    markerOffset: -15,
-  },
-  {
-    name: 'Moçambique',
-    coordinates: [32.575417159798555, -25.91817548744219],
-    markerOffset: -15,
-  },
+    {
+        name: "La Paz",
+        coordinates: [-68.08476053082477, -16.413786652845964],
+        markerOffset: -10,
+    },
+    {
+        name: "Miami",
+        coordinates: [-80.20268202730227, 25.764768118455407],
+        markerOffset: -10,
+    },
+    {
+        name: "Nova York",
+        coordinates: [-73.94401297179279, 40.7356621309984],
+        markerOffset: -9,
+    },
+    {
+        name: "Toronto",
+        coordinates: [-90.60809330413308, 48.68489624121235],
+        markerOffset: -10,
+    },
+    {
+        name: "Vancouver",
+        coordinates: [-123.11360638784217, 49.280340393447624],
+        markerOffset: -10,
+    },
+    {
+        name: "Santiago",
+        coordinates: [-70.64173954569011, -28.43632769633844],
+        markerOffset: -10,
+    },
+    {
+        name: "Rio de Janeiro",
+        coordinates: [-43.18921923638575, -22.927798424412373],
+        markerOffset: -10,
+    },
+    {
+        name: "Buenos Aires",
+        coordinates: [-58.381865707868904, -34.5877300047204],
+        markerOffset: -10,
+    },
+    {
+        name: "Colombo",
+        coordinates: [80.03887784919652, 7.173901570241348],
+        markerOffset: -10,
+    },
+    {
+        name: "San José",
+        coordinates: [-84.17711135248291, 9.689154878175138],
+        markerOffset: -10,
+    },
+    {
+        name: "Bogotá",
+        coordinates: [-73.6527840486543, 4.9512399114194805],
+        markerOffset: -10,
+    },
+    {
+        name: "Roma",
+        coordinates: [12.95443532192378, 41.852335890417049],
+        markerOffset: -10,
+    },
+    {
+        name: "Londres",
+        coordinates: [-0.14074530098284344, 51.497790976427304],
+        markerOffset: -10,
+    },
+    {
+        name: "Brasília",
+        coordinates: [-47.85924382365552, -10.71770015684776],
+        markerOffset: -10,
+    },
+    {
+        name: "Oslo",
+        coordinates: [10.761077452175183, 59.91340704946014],
+        markerOffset: -10,
+    },
+    {
+        name: "Tokyo",
+        coordinates: [139.73261791204246, 35.66258399079574],
+        markerOffset: -10,
+    },
+    {
+        name: "Singapura",
+        coordinates: [103.80704813966021, 1.261555954803802],
+        markerOffset: -10,
+    },
+    {
+        name: "Sydney",
+        coordinates: [150.91566629396763, -33.518286016815225],
+        markerOffset: -10,
+    },
+    {
+        name: "Hong Kong",
+        coordinates: [114.18811352358868, 22.323447816962556],
+        markerOffset: -10,
+    },
+    {
+        name: "Pequim",
+        coordinates: [116.38643462562045, 39.88061000499358],
+        markerOffset: -10,
+    },
+    {
+        name: "Xangai",
+        coordinates: [121.45186402647722, 30.204342182428128],
+        markerOffset: -10,
+    },
+    {
+        name: "Manila",
+        coordinates: [120.86730237129824, 12.754060738103458],
+        markerOffset: -10,
+    },
+    {
+        name: "Cidade do Cabo",
+        coordinates: [18.353715012552726, -34.14548723743959],
+        markerOffset: -10,
+    },
+    {
+        name: "Nairobi",
+        coordinates: [36.81167940997817, -1.3038914951063236],
+        markerOffset: -10,
+    },
+    {
+        name: "Angola",
+        coordinates: [13.228827895004587, -8.823925433761437],
+        markerOffset: -10,
+    },
+    {
+        name: "Moçambique",
+        coordinates: [32.575417159798555, -25.91817548744219],
+        markerOffset: -10,
+    },
+    {
+        name: "Moscou",
+        coordinates: [37.450785120841026, 55.99608324820399],
+        markerOffset: -10,
+    },
+    {
+        name: "Zhangye",
+        coordinates: [90.49795963286768, 45.1219594409581],
+        markerOffset: -10,
+    },
+    {
+        name: "Bagdá",
+        coordinates: [44.346750166036315, 33.54525235076959],
+        markerOffset: -10,
+    },
 ];
